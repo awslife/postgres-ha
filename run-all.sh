434e2e0b6e259e4959f3ba3d1995d31bbd6483ac
awslife <awslife.song@gmail.com>
2021-05-05 23:23:57 +0900

    add pgp

--- run-all.sh
+++ run-all.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
 ansible-playbook -i postgres-ha proxmox.yaml --forks=4 \
+  && ansible-playbook -i postgres-ha cluster.yaml --forks=4 \
   && ansible-playbook -i postgres-ha postgres.yaml --forks=2
